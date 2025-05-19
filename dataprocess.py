f3655156c094f5b0eb9450d8eebfcfb35b33f1bd
Michael J Zenz <mike@mjzenz.org>
2025-05-18 22:58:35 -0500

    Added first data process code.

--- dataprocess.py
+++ dataprocess.py
@@ -3,14 +3,11 @@ import pandas as pd
 import keyring
 import import_data
 
-fredapi_key = "key"
-
+fred_api_key = keyring.get_password("fredapi","fredapi")
 
 filenames = import_data.read_ufas_file_names("data")
-salary_data = import_data.clean_ufas_data(filenames, fredapi_key)
-# Example usage
-files = ["2021_12.xlsx", "2022_06.xlsx", "2024_01.xlsx"]
-fred_api_key = "YOUR_FRED_API_KEY"
-cleaned_data = clean_UFAS_Data(files, fred_api_key)
-print(cleaned_data.head())
 
+#Basic Data Cleaning
+salary_data = import_data.clean_ufas_data(filenames, fred_api_key)
+
+#Remove 0 FTE appointments
